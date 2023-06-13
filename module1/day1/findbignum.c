a709484f2f88b88cc48f7458b61af33395834bc4
Adithya <kavarthapuadithya91@gmail.com>
2023-06-13 20:24:49 +0530

    changed the code

---
+++ module1/day1/findbignum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int a, b;
+    
+    printf("Enter two numbers : ");
+    scanf("%d\n%d", &a, &b);
+
+    if(a>b)
+    {
+        printf("%d is greater number", a);
+    }
+
+    else if(a<b)
+    {
+        printf("%d is greater number", b);
+    }
+
+    else
+    {
+        printf("%d and %d are same values", a,b);
+    }
+    
+}
