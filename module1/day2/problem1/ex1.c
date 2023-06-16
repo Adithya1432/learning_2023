ee32661547207d3a1bf906cd17bdc9f80ba91b64
Adithya <kavarthapuadithya91@gmail.com>
2023-06-16 17:41:17 +0530

    Day 3 code added

--- module1/day2/problem1/ex1.c
+++ module1/day2/problem1/ex1.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
-#include <stdint.h>
 
-void printExponent(double num) {
-    uint64_t* ptr = (uint64_t*)&num;
-    uint64_t exponentMask = 0x7FF0000000000000ULL;
-    uint64_t exponent = (*ptr & exponentMask) >> 52;
+void print_exponent(double x) {
+  
+  unsigned long long *exponent_ptr = (unsigned long long *)&x;
 
-    printf("Exponent in hexadecimal: 0x%03llx\n", exponent);
+  
+  unsigned int exponent = (*exponent_ptr >> 52) & 0x7FF;
+  if (exponent == 0) {
+    printf("Exponent is subnormal\n");
+  } else if (exponent == 0x7FF) {
+    printf("Exponent is infinity or NaN\n");
+  } else {
+    printf("Exponent in hexadecimal: 0x%03x\n", exponent);
+  }
 
-    // Convert exponent to binary
-    printf("Exponent in binary: 0b");
-    for (int i = 10; i >= 0; --i) {
-        putchar((exponent >> i) & 1 ? '1' : '0');
-    }
-    printf("\n");
+  
+  unsigned int binary_exponent = exponent;
+  printf("Exponent in binary: 0b");
+  for (int i = 10; i >= 0; i--) {
+    printf("%d", (binary_exponent >> i) & 1);
+  }
+  printf("\n");
 }
 
 int main() {
-    double x = 0.7;
-    printExponent(x);
+  
+  double x;
+  printf("\n Enter the Positive decimal number");
+  scanf("%lf",&x);
+  print_exponent(x);
 
-    return 0;
+  double y;
+  printf("\n Enter the Negative decimal number");
+  scanf("%lf",&y);  
+  print_exponent(y);
+
+  return 0;
 }
